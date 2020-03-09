8d0b747abf14d8f301625ae2a63f6a7c61f78003
Richard Jones <richard@currenthealth.com>
2020-03-09 18:01:25 +0000

    [WIP] Bump version on event

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,70 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import * as core from '@actions/core';
+import fs from 'fs';
+import {WebhookPayloadPullRequest} from '@octokit/webhooks';
+import childProcess from 'child_process';
+import path from 'path';
+import simplegit, {SimpleGit} from 'simple-git/promise';
+
+const VERSION_BUMP_TRIGGERS = ['#major', '#minor', '#patch'];
 
 async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    const git: SimpleGit = simplegit();
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    await git.addConfig('user.name', core.getInput('username'));
+    await git.addConfig('user.email', core.getInput('email'));
 
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    core.setFailed(error.message)
+    try {
+        const eventName = core.getInput('event_name');
+        if (eventName !== 'pull_request') {
+            return;
+        }
+
+        const eventJson = core.getInput('event');
+        const payload: WebhookPayloadPullRequest = JSON.parse(eventJson);
+
+        let versionBump: string | undefined = undefined;
+
+        if (!(payload.action === 'closed' && payload.pull_request.merged)) {
+            return;
+        }
+
+        await git.checkout(payload.pull_request.base.ref);
+
+        // Check for `#major`/`#minor`/`#patch` in PR body
+        for (const trigger of VERSION_BUMP_TRIGGERS) {
+            if (payload.pull_request.body.includes(trigger)) {
+                versionBump = trigger.replace('#', '');
+                break;
+            }
+        }
+
+        // TODO: Check for `#major`/`#minor`/`#patch` in merge commit?
+
+        versionBump = versionBump || 'minor';
+
+        childProcess.execSync(`./gradlew -P release-type=${versionBump} bump-version`);
+
+        const tempDir = fs.mkdtempSync('gradle-scripts');
+        const tempFile = path.join(tempDir, 'init.gradle');
+        fs.writeFileSync(
+            tempFile,
+            `rootProject {
+  task 'dumpVersion' {
+    println version
   }
+}`
+        );
+        const newVersion = childProcess.execSync(`./gradlew -I ${tempFile} dumpVersion`).toString();
+        fs.rmdirSync(tempDir);
+
+        await git.commit(`Bumped ${versionBump} version to ${newVersion}`);
+        await git.push();
+
+        core.setOutput('newVersion', newVersion);
+        core.setOutput('versionBump', versionBump);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
 
-run()
+run();
